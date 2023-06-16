ea376687869870ec042e31fcdeecdbb502bb169e
Daniela Vornic <daniela.vornic@gmail.com>
2023-06-16 19:01:43 +0300

    Add chakra

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,29 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import { createRoot } from "react-dom/client";
+import "tailwindcss/tailwind.css";
+import App from "components/App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
+const container = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(container);
 
-root.render(<App />)
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#8cc7f0",
+      200: "#6fb2e6",
+      300: "#528dde",
+      400: "#3588d4",
+      500: "#337ab7",
+      600: "#2d68a3",
+      700: "#26568f",
+      800: "#1f447b",
+      900: "#183367",
+    },
+  },
+});
+
+root.render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>,
+);
