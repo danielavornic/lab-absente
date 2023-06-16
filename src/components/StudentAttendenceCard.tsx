7863e746f1efebdee6f831e7c92cbab32367cd7e
Gusev Roman <114294990+GhenntoggyUTM@users.noreply.github.com>
2023-06-17 02:39:00 +0300

    Update Student Attendance Card
    Added Modal

--- src/components/StudentAttendenceCard.tsx
+++ src/components/StudentAttendenceCard.tsx
@@ -1,4 +1,5 @@
-import { Avatar, HStack, Text, Badge, Box } from "@chakra-ui/react";
+import { Avatar, HStack, Text, Badge, Box, useDisclosure, Modal, ModalBody, ModalHeader, ModalOverlay, ModalContent, Button, ModalFooter, ModalCloseButton } from "@chakra-ui/react";
+import { stubObject } from "lodash";
 import { AbsentaStatus } from "types";
 
 interface StudentAttendenceCardProps {
@@ -18,6 +19,7 @@ export const StudentAttendenceCard = ({
   year,
   isChanged,
 }: StudentAttendenceCardProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const iconLetter = subject[0].toUpperCase();
   const iconColor = isChanged ? "red.500" : "brand.500";
   const statusColorScheme =
@@ -28,40 +30,66 @@ export const StudentAttendenceCard = ({
       : "yellow";
 
   return (
-    <HStack
-      spacing={4}
-      boxShadow="sm"
-      justifyContent="space-between"
-      rounded="md"
-      p={4}
-      bg="white"
-      cursor="pointer"
-      _hover={{
-        backgroundColor: "gray.50",
-      }}
-    >
-      <HStack spacing={4}>
-        <Avatar name={iconLetter} bg={iconColor} size="sm" />
-        <HStack spacing={2}>
-          <Text fontWeight="medium" w="320px">
-            {subject}
-          </Text>
-          <Text w="220px" color="gray.500">
-            Prof. {prof}
-          </Text>
-          <Text fontSize="sm" color="gray.500">
-            {year}
-          </Text>
+    <>
+      <HStack
+        spacing={4}
+        boxShadow="sm"
+        justifyContent="space-between"
+        rounded="md"
+        p={4}
+        bg="white"
+        cursor="pointer"
+        _hover={{
+          backgroundColor: "gray.50",
+        }}
+        onClick={onOpen}
+      >
+        <HStack spacing={4}>
+          <Avatar name={iconLetter} bg={iconColor} size="sm" />
+          <HStack spacing={2}>
+            <Text fontWeight="medium" w="320px">
+              {subject}
+            </Text>
+            <Text w="220px" color="gray.500">
+              Prof. {prof}
+            </Text>
+            <Text fontSize="sm" color="gray.500">
+              {year}
+            </Text>
+          </HStack>
+        </HStack>
+        <HStack spacing={6}>
+          <Text fontSize="sm">{count} absențe</Text>
+          <Box w="84px">
+            <Badge colorScheme={statusColorScheme} ml="auto" display="block" w="fit-content">
+              {status}
+            </Badge>
+          </Box>
         </HStack>
       </HStack>
-      <HStack spacing={6}>
-        <Text fontSize="sm">{count} absențe</Text>
-        <Box w="84px">
-          <Badge colorScheme={statusColorScheme} ml="auto" display="block" w="fit-content">
-            {status}
-          </Badge>
-        </Box>
-      </HStack>
-    </HStack>
+      <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Descriere</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Obiect: {subject}
+            <br />
+            Profesor: {prof}
+            <br />
+            Nr. absenţe: {count}
+            <br />
+            Statut: {status}
+            <br />
+            Anul: {year}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='brand' mr={3} onClick={onClose}>
+              Închide
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
