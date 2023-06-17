01ef35524c750194e24b9eae720076087691be6b
Daniela Vornic <daniela.vornic@gmail.com>
2023-06-17 12:21:58 +0300

    Update Noutati.tsx

--- src/pages/Noutati.tsx
+++ src/pages/Noutati.tsx
@@ -1,5 +1,17 @@
 import { Layout } from "components";
 
 export const Noutati = () => {
-  return <Layout title="Noutăți">News</Layout>;
+  return (
+    <Layout title="Noutăți">
+      <iframe
+        style={{
+          width: "100%",
+          height: "800px",
+        }}
+        src="https://utm.md/json/redj.php"
+      >
+        <p>Your browser does not support iframes.</p>
+      </iframe>
+    </Layout>
+  );
 };
